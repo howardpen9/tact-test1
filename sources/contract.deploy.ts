2b54cacfdff7ade70b8cbbcff4cb2c89558f4728
Howard Peng <howard.pen9@gmail.com>
2023-08-18 16:19:46 +0800

    initial update, added:body

--- sources/contract.deploy.ts
+++ sources/contract.deploy.ts
@@ -1,35 +1,44 @@
-import * as fs from 'fs';
-import * as path from 'path';
-import { Address, contractAddress } from "ton";
-import { SampleTactContract } from "./output/sample_SampleTactContract";
+import * as fs from "fs";
+import * as path from "path";
+import { Address, contractAddress, beginCell } from "ton";
+import { SampleTactContract, storeAdd } from "./output/sample_SampleTactContract";
 import { prepareTactDeployment } from "@tact-lang/deployer";
 
 (async () => {
-
     // Parameters
     let testnet = true;
-    let packageName = 'sample_SampleTactContract.pkg';
-    let owner = Address.parse('kQBM7QssP28PhrctDOyd47_zpFfDiQvv5V9iXizNopb1d2LB');
+    let packageName = "sample_SampleTactContract.pkg";
+    let owner = Address.parse("kQBM7QssP28PhrctDOyd47_zpFfDiQvv5V9iXizNopb1d2LB");
     let init = await SampleTactContract.init(owner);
 
     // Load required data
     let address = contractAddress(0, init);
+
+    let packed = beginCell()
+        .store(
+            storeAdd({
+                $$type: "Add",
+                amount: 2n,
+            })
+        )
+        .endCell();
+
     let data = init.data.toBoc();
-    let pkg = fs.readFileSync(path.resolve(__dirname, 'output', packageName));
+    let pkg = fs.readFileSync(path.resolve(__dirname, "output", packageName));
 
     // Prepareing
-    console.log('Uploading package...');
+    console.log("Uploading package...");
     let prepare = await prepareTactDeployment({ pkg, data, testnet });
 
     // Deploying
     console.log("============================================================================================");
-    console.log('Contract Address')
+    console.log("Contract Address");
     console.log("============================================================================================");
     console.log();
     console.log(address.toString({ testOnly: testnet }));
     console.log();
     console.log("============================================================================================");
-    console.log('Please, follow deployment link')
+    console.log("Please, follow deployment link");
     console.log("============================================================================================");
     console.log();
     console.log(prepare);
